260a90308506b02c30ea3af43a87ab9a050b60e1
Muhammed <luffymouhamed2003@hotmail.com>
2026-05-12 22:52:38 +0100

    Update clsGlobalSettings.cs

--- DVLD Presentation Layer/clsGlobalSettings.cs
+++ DVLD Presentation Layer/clsGlobalSettings.cs
@@ -1,14 +1,56 @@
-﻿using DVLDBusinessLayer;
+using DVLDBusinessLayer;
+using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DVLD
 {
     internal class clsGlobalSettings
     {
         public static clsUser CurrentUser;
+        public static string Username;
+        public static string Password;
+
+        public static void SaveCredentialsInRegistry(string Username, string Password)
+        {
+            string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
+
+            try
+            {
+                Registry.SetValue(keyPath, "Username", Username, RegistryValueKind.String);
+                Registry.SetValue(keyPath, "Password", Password, RegistryValueKind.String);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static bool LoadCredentialsFromRegistry()
+        {
+            string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
+
+            try
+            {
+                string usernameValue = Registry.GetValue(keyPath, "Username", null) as string;
+                string passwordValue = Registry.GetValue(keyPath, "Password", null) as string;
+
+                if (usernameValue != null && passwordValue != null)
+                {
+                    Username = usernameValue;
+                    Password = passwordValue;
+                    return true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+            return false;
+        }
     }
 }
