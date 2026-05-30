6d1a3dc18c145ecae84ab8981e7ee318a2a54001
Muhammed <luffymouhamed2003@hotmail.com>
2026-05-30 15:18:04 +0100

    Update clsUtil.cs

--- DVLD Presentation Layer/clsUtil.cs
+++ DVLD Presentation Layer/clsUtil.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 
 namespace DVLD
@@ -41,6 +43,18 @@ namespace DVLD
 
                 MessageBox.Show("Error copying image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
+            }
+        }
+
+        public static string ComputeHash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create()) //SHA = Secure Hash Algorithms.
+            {
+                // Computing hash value from the UTF-8 encoded input string
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convert the byte array to a lowercase hexadecimal string
+                return BitConverter.ToString(hashBytes).Replace("-", "");
             }
         }
     }
